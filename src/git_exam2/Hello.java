1a0a9bd734e4bf32fd84bee4727cbfdaf5cf05d5
Yang, GyeongPil <ykp9711@gmail.com>
2021-10-26 09:51:37 +0900

    201632013-3

--- src/git_exam2/Hello.java
+++ src/git_exam2/Hello.java
@@ -3,6 +3,7 @@ package git_exam2;
 public class Hello {
 
 	public static void main(String[] args) {
+		System.out.println("IT융합자율학부");
 		System.out.println("201632013");
 		System.out.println("양경필");
 	}
