c843ed9e1aa50d0ca131fc8514f1b1cfbc0884a0
Yang, GyeongPil <ykp9711@gmail.com>
2021-10-26 09:53:04 +0900

    201632013-4

--- src/git_exam2/World.java
+++ src/git_exam2/World.java
@@ -2,6 +2,13 @@ package git_exam2;
 
 public class World {
 
+	static int factorial(int n) {
+		int result = 1;
+		for (int i = 1; i <= n; ++i)
+			result *= i;
+		return result;
+	}
+
 	static int sum(int n) {
 		int result = 0;
 		for (int i = 1; i <= n; ++i)
@@ -12,5 +19,7 @@ public class World {
 	public static void main(String[] args) {
 		for (int i = 5; i <= 10; ++i)
 			System.out.println(sum(i));
+		for (int i = 5; i <= 10; ++i)
+			System.out.println(factorial(i));
 	}
 }
